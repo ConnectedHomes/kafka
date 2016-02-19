2da811fc9516728aa09a184453d8d4c5254cc89b
Josep Casals <joscas@gmail.com>
2016-02-19 17:04:25 +0100

    Adding hash partitioner

--- clients/src/main/java/org/apache/kafka/clients/producer/internals/Partitioner.java
+++ clients/src/main/java/org/apache/kafka/clients/producer/internals/Partitioner.java
@@ -16,10 +16,12 @@
  */
 package org.apache.kafka.clients.producer.internals;
 
+import java.math.BigInteger;
 import java.util.List;
 import java.util.Random;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.Cluster;
 import org.apache.kafka.common.PartitionInfo;
@@ -38,8 +40,39 @@ public class Partitioner {
     private final AtomicInteger counter = new AtomicInteger(new Random().nextInt());
 
     /**
+     * Multiplicative hashing, in which the hash index is computed as ⌊m * frac(ka)⌋.
+     * Here k is again an integer hash code, a is a real number and frac is the function that returns
+     * the fractional part of a real number.
+     *
+     * Multiplicative hashing sets the hash index from the fractional part of multiplying k by a large real number.
+     * Multiplicative hashing works well for the same reason that linear congruential multipliers generate apparently
+     * random numbers—it's like generating a pseudo-random number with the hashcode as the seed.
+     *
+     * The multiplier a should be large and its binary representation should be a "random" mix of 1's and 0's.
+     * Multiplicative hashing is cheaper than modular hashing because multiplication
+     * is usually considerably faster than division (or mod).
+     */
+    private static Double randomLookingRealNum = Math.sqrt(5);
+    private static int multiplicativeHash(int key, int buckets) {
+        return (int) Math.floor(buckets * (key * randomLookingRealNum % 1));
+    }
+
+    /*
+     * Utility methods to show the proper way
+     * to convert integers and strings
+     * to byte arrays for the purpose of this
+     * partitioner
+     */
+    public byte[] intToBytes(int key) {
+        return Integer.toString(Math.abs(key)).getBytes();
+    }
+    public byte[] stringToBytes(String key) {
+        return key.replace("-","").getBytes(); // Note we remove all '-' to avoid negative numbers
+    }
+
+    /**
      * Compute the partition for the given record.
-     * 
+     *
      * @param record The record being sent
      * @param cluster The current cluster metadata
      */
@@ -50,9 +83,9 @@ public class Partitioner {
             // they have given us a partition, use it
             if (record.partition() < 0 || record.partition() >= numPartitions)
                 throw new IllegalArgumentException("Invalid partition given with record: " + record.partition()
-                                                   + " is not in the range [0..."
-                                                   + numPartitions
-                                                   + "].");
+                        + " is not in the range [0..."
+                        + numPartitions
+                        + "].");
             return record.partition();
         } else if (record.key() == null) {
             int nextValue = counter.getAndIncrement();
@@ -65,8 +98,20 @@ public class Partitioner {
                 return Utils.abs(nextValue) % numPartitions;
             }
         } else {
-            // hash the key to choose a partition
-            return Utils.abs(Utils.murmur2(record.key())) % numPartitions;
+            /**
+             * hash the key to choose a partition
+             * Use BigInteger to convert bytes to int
+             * unless the string is numeric in which case
+             * we parse the integer.
+             **/
+            int keyInt;
+            String keyString = new String(record.key());
+            if (StringUtils.isNumeric(keyString)) {
+                keyInt = Integer.parseInt(keyString);
+            } else {
+                keyInt = new BigInteger(record.key()).intValue();
+            }
+            return Utils.abs(multiplicativeHash(keyInt,numPartitions));
         }
     }
 
