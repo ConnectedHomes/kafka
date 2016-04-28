00ae96f011ddb5cae29e430f7783bbba1d04c756
suksai <sukhdev.saini@bgch.co.uk>
2016-04-28 10:16:01 +0100

    Shell script to configure Mirror Maker run across different environments

--- bin/kafka-mirror-maker-test.sh
+++ bin/kafka-mirror-maker-test.sh
@@ -2,10 +2,11 @@
 
 base_dir=$(dirname $0)/..
 
+echo "Starting Mirror Maker on $@ environment"
 $base_dir/bin/kafka-run-class.sh kafka.tools.MirrorMaker\
-  --consumer.config $base_dir/config/consumer-source-mirror-maker.properties \
-  --producer.config $base_dir/config/producer-target-mirror-maker.properties \
+  --consumer.config $base_dir/config/$1/consumer-source-mirror-maker.properties \
+  --producer.config $base_dir/config/$1/producer-target-mirror-maker.properties \
   --whitelist="upstream" \
-  --targetTopic="targetUpstream" \
+  --targetTopicPrefix="prod" \
   --partitions="1..10"    \
   --new.consumer
